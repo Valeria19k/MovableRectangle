1bee80e0613781178b863ce96063dbd7e494319d
KimVal <71520455+Valeria19k@users.noreply.github.com>
2020-12-20 22:04:43 +0300

    Create MovablePoint.java

---
+++ MovablePoint.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class MovablePoint implements Movable {
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getxSpeed() {
+        return xSpeed;
+    }
+
+    public int getySpeed() {
+        return ySpeed;
+    }
+    public void moveUp() {
+        this.y++;
+    }
+
+    public void moveDown() {
+        this.y--;
+    }
+
+    public void moveRight() {
+        this.x++;
+    }
+
+    public void moveLeft() {
+        this.x--;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+}
