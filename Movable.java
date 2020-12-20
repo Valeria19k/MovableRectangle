976d27dce3078b29e19ca94818a5e877467c6a02
KimVal <71520455+Valeria19k@users.noreply.github.com>
2020-12-20 22:04:10 +0300

    Create Movable.java

---
+++ Movable.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface Movable {
+    public abstract void moveUp();
+
+    public abstract void moveDown();
+
+    public abstract void moveLeft();
+
+    public abstract void moveRight();
+}
